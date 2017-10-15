2dd2dc20d47d04f13c15a16a67a9d73253a41687
DuyDo <duydo.dev@gmail.com>
2017-10-15 18:20:23 -0500

    ...

---
+++ config/orm.js
@@ -0,0 +1,42 @@
+const connection = require("./connection.js");
+
+var orm = {
+    selectAll : function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result){
+            if(err) throw err;
+            cb(result);
+        });
+    },
+    // selectAllDevoured : function(tableInput, cb) {
+    //     var queryString = "SELECT * FROM " + tableInput + " WHERE devoured = 1" + ";";
+    //     connection.query(queryString, function(err, result){
+    //         if(err) throw err;
+    //         cb(result);
+    //     });
+    // },
+
+    insertOne: function(tableInput, burger_name, cb) {
+        var queryString = "INSERT INTO " + tableInput;
+        queryString += " (burger_name) VALUES ";
+        queryString += "('" + burger_name + "'" + ")" + ";";
+        connection.query(queryString, function(err, result){
+            if(err) throw err;
+            cb(result);
+        });
+    },
+
+    updateOne: function(tableInput, condition, cb){
+        var queryString = "UPDATE " + tableInput;
+        queryString += " SET devoured = 1" ;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result){
+            if(err) throw err;
+            cb(result);
+        });
+    }
+}
+
+module.exports = orm;
